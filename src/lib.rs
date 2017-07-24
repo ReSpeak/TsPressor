414a97d57c704419dfc0241ca56acbc2c7c38cad
Flakebi <flakebi@t-online.de>
2017-07-25 00:45:13 +0200

    Use commands to send messages

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,5 @@
 #[macro_use]
 extern crate ts3plugin;
-#[macro_use]
-extern crate lazy_static;
 extern crate base64;
 extern crate brotli2;
 extern crate clipboard;
@@ -39,92 +37,46 @@ fn ts3decompress(input: &str) -> Option<String> {
     }
 }
 
-fn sendback<S: AsRef<str>>(api: &TsApi, server: &Server, target: MessageReceiver, message: S) {
-    if let Err(err) = match target {
-        MessageReceiver::Connection(cid) => {
-            match server.get_connection(cid) {
-                Some(con) => con.send_message(message),
-                None => Err(Error::VsCritical),
-            }
-        }
-        MessageReceiver::Channel => {
-            match server.get_own_connection_id().ok()
-                .and_then(|c| server.get_connection(c)) {
-                Some(own_con) => {
-                    match own_con.get_channel_id().ok()
-                        .and_then(|c| server.get_channel(c)) {
-                        Some(chan) => chan.send_message(message),
-                        None => Err(Error::VsCritical),
-                    }
-                }
-                None => Err(Error::VsCritical),
-            }
-        }
-        MessageReceiver::Server => server.send_message(message),
-    } {
-        api.log_or_print(format!("Failed to send: {:?}", err),
-                         "tspressor",
-                         LogLevel::Info);
-    }
+fn sendmsg<S: AsRef<str>>(api: &TsApi, server: &Server, message: S) {
+    server.send_plugin_message(api.get_plugin_id(), message);
 }
 
 impl Plugin for TsPressor {
+    fn name()        -> String { String::from("TsPressor") }
+    fn version()     -> String { String::from("0.1.0") }
+    fn author()      -> String { String::from("Splamy") }
+    fn description() -> String { String::from("Lets you compress and send \
+        messages which are larger then 1024 chars between user which have this \
+        plugin") }
+    fn command() -> Option<String> { Some(String::from("press")) }
+
     fn new(api: &mut TsApi) -> Result<Box<Self>, InitError> {
         api.log_or_print("Inited", "tspressor", LogLevel::Info);
         Ok(Box::new(TsPressor))
-        // Or return Err(InitError::Failure) on failure
     }
 
-    // Implement callbacks here
-    fn message(&mut self,
-               api: &mut TsApi,
-               server_id: ServerId,
-               invoker: Invoker,
-               target: MessageReceiver,
-               message: String,
-               _: bool)
-               -> bool {
-        let server = api.get_server(server_id);
-        if let Some(server) = server {
-            let own_id = server.get_own_connection_id();
-
-            if Ok(invoker.get_id()) == own_id {
-                // own message
-                if message.starts_with("--pack") {
-                    if let Ok(clip) = ClipboardContext::new().unwrap().get_contents() {
-                        if let Some(compresed_output) = ts3compress(&clip) {
-                            sendback(api, server, target, compresed_output);
-                            api.print_message("Ok");
-                        } else {
-                            api.log_or_print("failed to compress", "tspressor", LogLevel::Info);
-                        }
-                    } else {
-                        api.log_or_print("failed to get clip", "tspressor", LogLevel::Info);
-                    }
-                    return true;
-                }
+    fn process_command(&mut self, api: &mut TsApi, server_id: ServerId,
+        _: String) -> bool {
+        let server = api.get_server(server_id).unwrap();
+        if let Ok(clip) = ClipboardContext::new().unwrap().get_contents() {
+            if let Some(compresed_output) = ts3compress(&clip) {
+                sendmsg(api, server, compresed_output);
+                api.print_message("Ok");
             } else {
-                if let Some(decomp_str) = ts3decompress(&message) {
-                    api.print_message(decomp_str);
-                    return true;
-                }
+                api.log_or_print("Failed to compress", "tspressor", LogLevel::Info);
             }
         } else {
-            api.log_or_print("Message from no server", "tspressor", LogLevel::Info);
+            api.log_or_print("Failed to get clipboard", "tspressor", LogLevel::Info);
         }
-        false
+        true
     }
 
-    fn shutdown(&mut self, _: &mut TsApi) {
-        // api.log_or_print("TsPressor says goodbyte!", "tspressor", LogLevel::Info);
+    fn plugin_message(&mut self, api: &mut TsApi, _: ServerId,
+        _: String, message: String) {
+        if let Some(decomp_str) = ts3decompress(&message) {
+            api.print_message(decomp_str);
+        }
     }
 }
 
-create_plugin!("TsPressor",
-               "0.1.0",
-               "Splamy",
-               "Lets you compress and send messages which are larger then 1024 chars between \
-                user which have this plugin",
-               ConfigureOffer::No,
-               false,
-               TsPressor);
+create_plugin!(TsPressor);
